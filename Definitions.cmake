1286c5919a891b4d21922eaf231eaedc801e8b08
shiloxyz <shiloxyz@q6600arch>
2015-07-10 18:23:28 +0300

    init commit

---
+++ Definitions.cmake
@@ -0,0 +1,12 @@
+
+# Define project your name
+SET(Project "MyProject")
+
+# Project description
+SET(Project_desc "Template for qml cpp cmake projects")
+
+# Set copyright info
+SET(Copyright "Ololoshka")
+
+# Set Qt minimal version
+SET(Qt_min_REQUIRED 5.2.0)
